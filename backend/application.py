d1b9de62eda8636c77e0fd039b31d5550834a1dd
Hisao <32875647+jaimehisao@users.noreply.github.com>
2022-10-17 17:53:28 -0500

    New main app file

--- backend/application.py
+++ backend/application.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-### Post Routes ###
+############### Post Routes ###############
 @app.get("/posts/user")
 def get_all_posts_from_user(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
@@ -21,7 +21,22 @@ def get_post_by_id(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-### Comment Routes ###
+@app.post("/post/like")
+def get_post_by_id(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/post/unlike")
+def get_post_by_id(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.delete("/post/delete")
+def delete_post(item_id: int, q: Union[str, None] = None):
+    return None
+
+
+############### Comment Routes ###############
 @app.get("/comments/post")
 def create_new_comment_in_post():
     return None
@@ -32,6 +47,11 @@ def delete_comment_in_post():
     return None
 
 
-@app.put("/comments/like")
+@app.post("/comments/like")
+def like_comment_in_post():
+    return None
+
+
+@app.delete("/comments")
 def like_comment_in_post():
     return None
