245c0e285f3b33625ade454af57ee396ce0fbf34
Hisao <32875647+jaimehisao@users.noreply.github.com>
2022-10-18 18:34:25 -0500

    Changed route spec

--- backend/application.py
+++ backend/application.py
@@ -7,51 +7,51 @@ app = FastAPI()
 
 ############### Post Routes ###############
 @app.get("/posts/user")
-def get_all_posts_from_user(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def get_all_posts_from_user(user_id: int):
+    pass
 
 
 @app.get("/post/id")
-def get_post_by_id(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def get_post_by_id(post_id: int):
+    pass
 
 
 @app.post("/post/new")
-def get_post_by_id(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def create_new_post(post_id: int):
+    pass
 
 
 @app.post("/post/like")
-def get_post_by_id(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def like_post(post_id: int):
+    pass
 
 
 @app.post("/post/unlike")
-def get_post_by_id(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def unlike_post(post_id: int):
+    pass
 
 
 @app.delete("/post/delete")
-def delete_post(item_id: int, q: Union[str, None] = None):
-    return None
+def delete_post(post_id: int):
+    pass
 
 
 ############### Comment Routes ###############
 @app.get("/comments/post")
-def create_new_comment_in_post():
-    return None
+def create_new_comment_in_post(post_id: int, comment_body: str):
+    pass
 
 
 @app.delete("/comments/post")
-def delete_comment_in_post():
+def delete_comment_in_post(post_id: int, comment_id: int):
     return None
 
 
 @app.post("/comments/like")
-def like_comment_in_post():
+def like_comment_in_post(post_id: int, comment_id: int):
     return None
 
 
 @app.delete("/comments")
-def like_comment_in_post():
+def delete_comment_in_post(post_id: int, comment_id: int):
     return None
