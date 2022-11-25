24b7d6980455d0dce4f89734329ea4e934746da2
Hisao <32875647+jaimehisao@users.noreply.github.com>
2022-11-25 16:51:38 -0600

    Changed program structure

--- backend/database.py
+++ backend/database.py
@@ -5,72 +5,37 @@ from dto.comment import Comment
 
 import logging
 
-client = MongoClient('mongo', 27017)
-
-post_db = client['posts']
-user_db = client['users']
-
-
-def get_user(user_id: str):
-    logging.info("Getting user with id: " + user_id)
-    return user_db.users.find_one({"user_id": user_id})
+client = MongoClient("localhost", 27017)
+# client = MongoClient('mongo', 27017)
+# database
+database = client["exchangeagram"]
+post_db = database["posts"]
+user_db = database["users"]
 
 
-def get_user_posts(user_id: str):
-    logging.info("Getting posts from user with id: " + user_id)
-    return post_db.find_all({"user_id": user_id})
+def convert_post_to_dict(post: Post):
+    new_comments = []
+    for comment in post.comments:
+        new_comments.append(comment.dict())
+    post.comments = new_comments
+    dict_post = dict(post)
+    return dict_post
 
 
-def get_post_by_id(post_id: str):
-    logging.info("Getting post with id: " + post_id)
-    post = post_db.find_one({"id": post_id})
-    post = Post(post_id=post['id'],
-                body=post['body'],
-                image_path=post['image'],
-                user_id=post['user_id'],
-                created_at=post['created_at'])
-    return post
+def get_post_by_id_db(post_id: str):
+    logging.info("Getting post with id from DB: " + post_id)
+    post = post_db.find_one({"_id": post_id})
+    return Post.parse_obj(post)
 
 
-def create_new_comment_in_post(post: Post, comment: Comment):
-    logging.info("Creating new comment in post with id: " + str(post.id))
-    post.comments.append(comment)
-    return post_db.update_one({"id": post.id}, post)
+def update_post(post: Post):
+    logging.info("Updating post with id: " + post.id)
+    post = convert_post_to_dict(post)
+    post_db.update_one({"_id": post["id"]}, post)
 
 
-def like_post(post_id):
-    logging.info("Liking post with id: " + post_id)
-    post = post_db.find_one({"id": post_id})
-    # case where post not found
-    post['likes'] = post['likes']+1
-    post_db.update_one({"id": post.id}, post)
-
-
-def unlike_post(post_id):
-    logging.info("Unliking post with id: " + post_id)
-    post = post_db.find_one({"id": post_id})
-    if post['likes'] > 0:
-        post['likes'] = post['likes'] - 1
-        post_db.update_one({"id": post.id}, post)
-        return
-    logging.info("Post has no likes")
-
-
-def create_new_comment(post_id, comment: Comment):
-    logging.info("Creating new comment in post with id: " + post_id)
-    post = post_db.find_one({"id": post_id})
-    # case where post not found
-    post['comment'].append(comment)
-
-
-"""
-def create_new_post(post: Post):
-    logging.info("Creating new post")
-    return post_db.insert_one(post)
-
-
-def delete_post(post_id: str):
-    logging.info("Deleting post with id: " + post_id)
-    # TODO validate post ownership
-    post_db.delete_one({"id": post_id})
-"""
+def post_upsert(post: Post):
+    logging.info("Upserting post with id: " + post.id)
+    post = convert_post_to_dict(post)
+    dict_post = convert_post_to_dict(post)
+    post_db.replace_one({"_id": dict_post["id"]}, dict_post, upsert=True)
---
+++ backend/operations.py
@@ -0,0 +1,96 @@
+from pymongo import MongoClient
+
+from dto.post import Post
+from dto.comment import Comment
+
+from database import get_post_by_id_db, convert_post_to_dict, update_post, post_upsert
+
+import logging
+
+
+def get_post_by_id(post_id: str) -> Post or None:
+    """
+    Gets a post by its ID.
+    :param post_id: The ID of the post to be retrieved.
+    :return: The Post object with the given ID.
+    """
+    logging.info("Getting post with id: " + post_id)
+    post = get_post_by_id_db
+    if post is None:
+        return None
+    return post
+
+
+def create_new_comment_in_post(post_id: str, comment: Comment):
+    """
+    Creates a new comment in a post
+    :param post_id: The ID of the post where the comment will be added.
+    :param comment: Comment that will be added to the post.
+    :return: Returns the status of the operation, none if negative.
+    """
+    post = get_post_by_id_db(post_id)
+    if post is None:
+        return False
+    post = convert_post_to_dict(post)
+    logging.info("Creating new comment in post with id: " + str(post["id"]))
+    post["comments"].append(dict(comment))
+    update_post(post)
+    return True
+
+
+def like_post(post_id: str):
+    logging.info("Liking post with id: " + post_id)
+    post = get_post_by_id_db(post_id)
+    if post is None:
+        return False   # In case of an unnsucesful operation
+    convert_post_to_dict(post)
+    post["likes"] = post["likes"] + 1
+    update_post(post)
+    return True  # If operation is successful
+
+
+def unlike_post(post_id: str) -> None:
+    logging.info("Unliking post with id: " + post_id)
+    post = post_db.find_one({"id": post_id})
+    if post["likes"] > 0:
+        post["likes"] = post["likes"] - 1
+        update_post(post)
+        return
+    logging.info("Post has no likes")
+
+
+def create_new_comment(post_id, comment: Comment):
+    logging.info("Creating new comment in post with id: " + post_id)
+    post = get_post_by_id_db(post_id)
+    if post is None:
+        return False
+    post.comments.append(comment)
+    update_post(post)
+
+
+def create_new_post(post: Post):
+    logging.info("Creating new post")
+    print(post)
+    post_upsert(post)
+
+
+"""
+def delete_post(post_id: str):
+    logging.info("Deleting post with id: " + post_id)
+    # TODO validate post ownership
+    post_db.delete_one({"id": post_id})
+    
+def get_user(user_id: str):
+    logging.info("Getting user with id: " + user_id)
+    user = user_db.users.find_one({"user_id": user_id})
+    if user:
+        return user
+    return None
+
+def get_user_posts(user_id: str):
+    logging.info("Getting posts from user with id: " + user_id)
+    posts = post_db.find({"user_id": user_id})
+    if posts is None:
+        return None
+    return post_db.find_all({"user_id": user_id})
+"""
